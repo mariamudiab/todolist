68b478b80fc0e5c1982d987b78bd3c07a4f504aa
mariamudiab <mariamudiab@gmail.com>
2025-08-30 15:30:15 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+let input = document.getElementById("todo-input")
+let submit = document.getElementById(".add-btn")
+
+let todoListDiv = document.getElementById(".todo-list")
+let clearAllBtn = document.getElementById(".clear-btn")
+
+let todoArray = []
+
